9b0353edf75549cdab3c02556e3bc63b858a7d2c
Ryan Yoo <ryan.yoo@avanade.com>
2018-08-29 16:22:07 -0500

    Add project files.

---
+++ WebScrapingExample/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace WpfTutorialSamples.Misc_controls
+{
+    public partial class WebBrowserControlSample : Window
+    {
+        public WebBrowserControlSample()
+        {
+            InitializeComponent();
+            wbSample.Navigated += (a, b) => { HideScriptErrors(wbSample, true); };
+            wbSample.Navigate("http://www.wpf-tutorial.com");
+            //var wb = new WebBrowser();
+            //wb.Navigate("http://www.wpf-tutorial.com");
+            //wb.Navigated += (a, b) => { HideScriptErrors(wb, true); };
+            
+        }
+
+        private void txtUrl_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+                wbSample.Navigate(txtUrl.Text);
+        }
+
+        private void wbSample_Navigating(object sender, System.Windows.Navigation.NavigatingCancelEventArgs e)
+        {
+            txtUrl.Text = e.Uri.OriginalString;
+        }
+
+        private void BrowseBack_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = ((wbSample != null) && (wbSample.CanGoBack));
+        }
+
+        private void BrowseBack_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            wbSample.GoBack();
+        }
+
+        private void BrowseForward_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = ((wbSample != null) && (wbSample.CanGoForward));
+        }
+
+        private void BrowseForward_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            wbSample.GoForward();
+        }
+
+        private void GoToPage_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void GoToPage_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            wbSample.Navigate(txtUrl.Text);
+        }
+
+        public void HideScriptErrors(WebBrowser wb, bool hide)
+        {
+            var fiComWebBrowser = typeof(WebBrowser).GetField("_axIWebBrowser2", BindingFlags.Instance | BindingFlags.NonPublic);
+            if (fiComWebBrowser == null) return;
+            var objComWebBrowser = fiComWebBrowser.GetValue(wb);
+            if (objComWebBrowser == null)
+            {
+                wb.Loaded += (o, s) => HideScriptErrors(wb, hide); //In case we are to early
+                return;
+            }
+            objComWebBrowser.GetType().InvokeMember("Silent", BindingFlags.SetProperty, null, objComWebBrowser, new object[] { hide });
+        }
+    }
+}
